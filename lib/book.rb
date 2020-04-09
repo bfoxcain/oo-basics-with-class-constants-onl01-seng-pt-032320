53405acf3e96f1bf7be7cbb683deb04fbb639514
Beverly Appleton <bfcain19@gmail.com>
2020-04-09 16:44:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   GENRES = []
 
@@ -11,17 +11,8 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  describe 'GENRES' do
-    it 'keeps track of all genres' do
-      genres = ["Thriller", "Science Fiction", "Romance"]
-      genres.each_with_index do |genre, i|
-        book = Book.new("Book_#{i}")
-        book.genre = genre
-    end
-    genres.each do |genre|
-      expect(Book::GENRES).to
-  include(genre)
-end
-end
-end
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
